162727b9ad038fd5ce9f872d132b20624289c306
fauzisho <fauzisholichin@gmail.com>
2019-06-26 12:57:20 +0800

    first init

---
+++ README.md
@@ -0,0 +1,33 @@
+
+## VIVMER Architecture Swift
+
+
+## How to install
+
+### Using script
+Only need execute this command in terminal:
+```swift
+sudo swift install.swift
+```
+
+## How to use
+
+
+Click Right Folder -> Click New File -> VIVMER
+
+
+### Architecture Menu
+
+- [x] None
+- [x] Xib
+- [x] StoryBoard
+- [ ] SwiftUI
+
+### Reference 
+
+### Contribution
+
+[![CC0](http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/cc-zero.svg)](https://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, [Fauzi Sholichin](https://github.com/fauzisho) has waived all copyright and related or neighboring rights to this work.
+
---
+++ install.swift
@@ -0,0 +1,65 @@
+//  Created by Fauzi Sholichin on 23/02/2019.
+//  Copyright © 2019 Fauzi Sholichin. All rights reserved.
+//
+
+import Foundation
+
+let templateVIVMER = "VIVMER.xctemplate"
+
+let destinationRelativePath = "/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application"
+
+func printInConsole(_ message:Any){
+    print("====================================")
+    print("\(message)")
+    print("====================================")
+}
+
+func moveTemplate(){
+
+    let fileManager = FileManager.default
+    let destinationPath = bash(command: "xcode-select", arguments: ["--print-path"]).appending(destinationRelativePath)
+    do {
+        if !fileManager.fileExists(atPath:"\(destinationPath)/\(templateVIVMER)"){
+        
+            try fileManager.copyItem(atPath: templateVIVMER, toPath: "\(destinationPath)/\(templateVIVMER)")
+            
+            printInConsole("✅  Architecture Kit VIVMER installed succesfully 🎉. Enjoy it 🙂")
+            
+        }else{
+            
+            try _ = fileManager.replaceItemAt(URL(fileURLWithPath:"\(destinationPath)/\(templateVIVMER)"), withItemAt: URL(fileURLWithPath:templateVIVMER))
+            
+            printInConsole("✅  Architecture Kit VIVMER already exists. So has been replaced succesfully 🎉. Enjoy it 🙂")
+        }
+    }
+    catch let error as NSError {
+        printInConsole("❌  Ooops! Something went wrong 😡 : \(error.localizedFailureReason!)")
+    }
+}
+
+func shell(launchPath: String, arguments: [String]) -> String
+{
+    let task = Process()
+    task.launchPath = launchPath
+    task.arguments = arguments
+    
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: String.Encoding.utf8)!
+    if output.characters.count > 0 {
+        //remove newline character.
+        let lastIndex = output.index(before: output.endIndex)
+        return String(output[output.startIndex ..< lastIndex])
+    }
+    return output
+}
+
+func bash(command: String, arguments: [String]) -> String {
+    let whichPathForCommand = shell(launchPath: "/bin/bash", arguments: [ "-l", "-c", "which \(command)" ])
+    return shell(launchPath: whichPathForCommand, arguments: arguments)
+}
+
+moveTemplate()
